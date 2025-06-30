d70cfa1e92ff7180c0815f48fb51c87bfc0f51d5
Sonam Lhamo <sonam.lhamo@connect.qut.edu.au>
2025-06-30 18:55:06 +0600

    updated

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     EscrowVerification: {
-      address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+      address: "0x4A679253410272dd5232B3Ff7cF5dbB88f295319",
       abi: [
         {
           inputs: [
@@ -358,7 +358,7 @@ const deployedContracts = {
       },
     },
     LandNFT: {
-      address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+      address: "0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f",
       abi: [
         {
           inputs: [
@@ -1069,7 +1069,7 @@ const deployedContracts = {
       },
     },
     LandRegistry: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      address: "0x3Aa5ebB10DC797CAC828524e59A333d0A371443c",
       abi: [
         {
           inputs: [
@@ -1907,7 +1907,7 @@ const deployedContracts = {
       },
     },
     VerifiersAndApprovers: {
-      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      address: "0xc6e7DF5E7b4f2A278906862b61205850344D4e7d",
       abi: [
         {
           anonymous: false,
@@ -2304,7 +2304,7 @@ const deployedContracts = {
       },
     },
     VerifierSetup: {
-      address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      address: "0x59b670e9fA9D0A427751Af201D676719a970857b",
       abi: [
         {
           inputs: [
